d47292a27a391d73f04153e22c97a18be14dacb9
thienvu <thien.vu@softfoundry.com>
2021-11-18 21:04:50 +0700

    add utilities file for draw the view button

--- datingApp/datingApp/LoginView/MainLoginViewController.swift
+++ datingApp/datingApp/LoginView/MainLoginViewController.swift
@@ -6,3 +6,30 @@
 //
 
 import Foundation
+import UIKit
+
+class loginViewController : ViewController {
+    
+    //init var
+    var userName:NSString?
+    var passWord:NSString?
+    
+    //labelView
+    
+    
+    @IBAction func signInButton(_ sender: Any) {
+        
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        //your code
+        setupElementView()
+    }
+    
+    func setupElementView() {
+        
+        
+    
+    }
+}
--- datingApp/datingApp/Utilities/ActionUtil.swift
+++ datingApp/datingApp/Utilities/ActionUtil.swift
@@ -6,3 +6,51 @@
 //
 
 import Foundation
+import UIKit
+
+class ActionUtilities : NSObject {
+    
+    var shareInstance:ActionUtilities?
+    
+    func getInstance() {
+        shareInstance! = ActionUtilities.init()
+        
+    }
+    
+    func utilButtonWhiteView (_ buttionView : UIButton ,_ backgroundColor : UIColor ) {
+        let buttonLayer = CALayer()
+        
+        buttonLayer.borderColor = UIColor.white.cgColor
+        buttonLayer.cornerRadius = 5
+        
+        buttionView.backgroundColor = backgroundColor
+        
+        buttionView.layer.addSublayer(buttonLayer)
+        
+    }
+    
+    func utilLabelView(_ labelView: UILabel,_ backgroundColor : UIColor ) {
+        
+        let labelLayer = CALayer()
+        
+        labelLayer.borderColor = UIColor.gray.cgColor
+        labelLayer.cornerRadius = 5
+        
+        labelView.backgroundColor = backgroundColor
+        
+        labelView.layer.addSublayer(labelLayer)
+    }
+    
+    func utilTextfieldView(_ textView: UITextField,_ backgroundColor : UIColor ) {
+        
+        let textLayer = CALayer()
+        
+        textLayer.borderColor = UIColor.gray.cgColor
+        textLayer.cornerRadius = 5
+        
+        textView.backgroundColor = backgroundColor
+            
+        textView.layer.addSublayer(textLayer)
+    }
+    
+}
